07b8822f81db37c13648b290a9b938f45c225937
Liz P <parisielizabeth@gmail.com>
2021-03-15 06:35:22 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import RestaurantInput from './components/RestaurantInput';
 import Restaurants from './components/Restaurants';
 import { connect } from 'react-redux';
-import 
+import { addRestaurant } from './actions/restaurants';
 
 export class App extends Component {
   render() {
